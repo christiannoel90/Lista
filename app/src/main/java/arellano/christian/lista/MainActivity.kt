6876dbf2c8d99ba824c831c618cb94ac1b9ddf4f
JOSUE ABRAHAM MENDOZA COVARRUBIAS <213769702@academicos.valles.udg.mx>
2019-10-23 17:35:21 -0500

    Initial commit

---
+++ app/src/main/java/arellano/christian/lista/MainActivity.kt
@@ -0,0 +1,12 @@
+package arellano.christian.lista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
